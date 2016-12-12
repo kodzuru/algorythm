1cb0b411023c54e5e3eb206256aee495ac6c85a3
kodzuru <kodzuru@tut.by>
2016-12-13 01:38:25 +0300

    v 0.00
    Chapter 1

---
+++ algorythm_00/algorythm_00.cpp
@@ -0,0 +1,27 @@
+﻿// algorythm_00.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+int main(){
+	setlocale(0, "Russian");
+	system("TITLE Неопознанный консольный объект");
+	system("color 2");
+
+	const int N = 10000;
+	int id[N], p, q;
+	for (int i = 0; i < N; i++) id[i] = i;
+	while (cin >> p >> q)
+	{
+		int t = id[p];
+		if (t == id[q]) continue;
+		for (int i = 0; i < N; i++)
+			if (id[i] == t) id[i] = id[q];
+		cout << " " << p << " " << q << endl;
+	}
+
+	_getch();
+    return 0;
+}
+
