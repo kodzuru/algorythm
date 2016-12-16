852d15184277c84965342ebce8c2dff608c782ac
kodzuru <kodzuru@tut.by>
2016-12-16 20:06:47 +0300

    v 0.00
    chapter_1

--- algorythm_00/algorythm_00.cpp
+++ algorythm_00/algorythm_00.cpp
@@ -9,54 +9,75 @@ int main(){
 	system("TITLE Неопознанный консольный объект");
 	system("color 2");
 
-	// алгоритм взвешенного быстрого соединения(weighted quick-union with path compression by halving algorythm)
-	const int N = 10;
-	int id[N], sz[N], p, q; // массив ID-все числа попорядку , p-q пара, SZ - массив с количеством связей в дереве
 
-	// заполняем массив ID - 0...N-1, SZ - 1(в начале у каждого корня нет веток)
-	for (int i = 0; i < N; i++) {
+	/*
+	//------------------1.4 
+	//p-q pair{0-2,1-4,2-5,3-6,0-4,1-3}		
+	// обращение к ID - 13
+	const int N = 10;
+	int i, p, q, id[N];
+	unsigned int counter = 0;
+
+	for (i = 0; i < N; i++) {
 		id[i] = i;
-		sz[i] = 1;
 		cout << id[i] << " ";
 	}
-
 	cout << endl;
-	/*
-	В массив ID запоминаются пары p-q. Если следующая пара подходит, то q заменяется на p.
-	Выполняется до тех пор, пока все ID не будут равны.
-	*/
-	while (cin >> p >> q)//вводим пару
-	{
 
-		int i, j;
-		// делаем дерево плоским
-		for (i = p; i != id[i]; i = id[i]) {
-			id[i] = id[id[i]]; // Уменьшем пути пополам
-		};
-		for (j = q; j != id[j]; j = id[j]) {
-			id[j] = id[id[j]]; // уменьшаем пути пополам
-		};
-		if (i == j) { cout << -1 << endl; continue; } // если p = q переходим к следующей итерации (если связь УЖЕ есть!)
 
-		// выбираем необходимое дерево
-		//(в начале разбиваем на много маленьких деревьев и если в конце деревья стыкуются, то делаем одно большое)
-		if (sz[i] < sz[j]) { // закидываем в дерево с самой длинной цепочкой
-			id[i] = j; sz[j] += sz[i];
-		}
-		else { // закидываем в любое подходящее дерево
-			id[j] = i; sz[i] += sz[j];
-		}
+	while (cin >> p >> q){
+		int t = id[p];
 
-		for (int i = 0; i < N; i++) {
-			cout << sz[i] << " ";
+		if (t == id[q]) continue;
+
+		for (i = 0; i < N; i++) {
+			if (id[i] == t) {
+				cout << "id[" << i << "] " << id[i] << endl;
+				id[i] = id[q];
+				counter++;
+			}
 		}
 		cout << endl;
+
 		for (int i = 0; i < N; i++) {
 			cout << id[i] << " ";
 		}
 		cout << endl;
-
+		cout << "Обратились к массиву: " << counter << " раз!" << endl;
 	}
+	*/
+
+	/*
+	//------------------1.5
+	//p-q pair{0-2,1-4,2-5,3-6,0-4,1-3}
+	// обращение к ID - 4
+	const int N = 10;
+	int p, q, id[N];
+	unsigned int counter = 0;
+
+	for (int i = 0; i < N; i++) {
+		id[i] = i;
+		cout << id[i] << " ";
+	}
+	cout << endl;
+
+
+	while (cin >> p >> q) {
+
+		int i, j;		
+		for (i = p; i != id[i]; i = id[i]) { counter++; }
+		for (j = q; j != id[j]; j = id[j]) { counter++; }
+		if (i == j) continue;
+		id[i] = j;
+		
+		for (int i = 0; i < N; i++) {
+			cout << id[i] << " ";
+		}
+		cout << endl;
+		cout << "Обратились к массиву: " << counter << " раз!" << endl;
+	}
+	*/
+
 
 	_getch();
     return 0;
